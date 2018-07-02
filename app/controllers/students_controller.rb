d78000768f52b0bc98a0116379b323155d9fbe17
Yew Woei <yewwoei.low@outlook.com>
2018-07-02 18:00:41 +0300

    create signup page and form error page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,28 @@
 class StudentsController < ApplicationController
   def new
+    @student = Student.new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      # handle successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:name, :email, :password,
+                                    :password_confirmation,
+                                    :phone_number,
+                                    :education_level,
+                                    :dob)
+    end
 end
