89a41d05a1007ebf83602eef3869970e3b7e7ec6
Yew Woei <yewwoei.low@outlook.com>
2018-07-02 16:55:10 +0300

    add validation for correct emmail format

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 class Student < ApplicationRecord
+    # ensures that the name is within 50 characters.
     validates(:name, presence: true, length: { maximum: 50})
+    # ensures email within 255 characters with valid format.
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }
 
-    validates :email, presence: true, length: {maximum: 255}
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -32,5 +32,23 @@ class StudentTest < ActiveSupport::TestCase
     @student.email = "a" * 244 + "@example.com"
     assert_not @student.valid?
   end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @student.email = valid_address
+      assert @student.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @student.email = invalid_address
+      assert_not @student.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 
 end
