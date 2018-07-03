fa1bb8fb76fecf9d945b25ec3b18c8fe13780cd8
Jordan Liew <jliew008@e.ntu.edu.sg>
2018-07-03 04:13:13 +0000

    Add student login test

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,5 +10,12 @@ class Student < ApplicationRecord
 
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-
+    
+    # Returns the hash digest of the given string.
+    def Student.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+    
 end
--- test/integration/students_login_test.rb
+++ test/integration/students_login_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class StudentLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @student = students(:michael)
+  end
   
   test "login with invalid information" do
     get login_path
@@ -10,6 +13,18 @@ class StudentLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  
+test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @student.email,
+                                          password: 'password' } }
+    assert_redirected_to @student
+    follow_redirect!
+    assert_template 'students/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", student_path(@student)
   end
 
 end
