ba4e5db4357eb4d46dd5862e2e4cd891a0e3576b
Jordan Liew <jliew008@e.ntu.edu.sg>
2018-07-03 02:34:44 +0000

    Catch invalid submissions for login

--- app/controllers/student_sessions_controller.rb
+++ app/controllers/student_sessions_controller.rb
@@ -1,4 +1,19 @@
 class StudentSessionsController < ApplicationController
   def new
   end
+  
+  def create
+    student = Student.find_by(email: params[:session][:email].downcase)
+    if student && student.authenticate(params[:session][:password])
+      # Log the student in and redirect to the sudent's show page.
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    
+  end
+  
 end
