2b69773350019da32354bc5f4d95853e850d480e
Jordan Liew <jliew008@e.ntu.edu.sg>
2018-07-03 02:09:41 +0000

    New student_session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'student_sessions/new'
   get 'students/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
